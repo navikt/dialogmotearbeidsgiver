94d9f6d5c58047796a8e2fc61be8294ecb1e786b
Erik Jansen <erik.gunnar.jansen@nav.no>
2019-09-10 14:46:21 +0200

    Fix link to Oppfolgingsplanarbeidsgiver

--- js/utils/urlUtils.js
+++ js/utils/urlUtils.js
@@ -11,7 +11,7 @@ const erHerokuApp = () => {
 };
 
 export const hentOppfolgingsplanarbeidsgiverUrl = (koblingId) => {
-    const sluttUrl = `${process.env.REACT_APP_OPPFOLGINGSDIALOG_CONTEXT_ROOT}/${koblingId}/oppfolgingsplaner`;
+    const sluttUrl = `${process.env.REACT_APP_OPPFOLGINGSPLAN_CONTEXT_ROOT}/${koblingId}/oppfolgingsplaner`;
     return toggleErPaaHeroku()
         ? `https://oppfolgingsplanarbeidsgiver.herokuapp.com${sluttUrl}`
         : sluttUrl;
