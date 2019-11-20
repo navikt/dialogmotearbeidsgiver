1e1aca8e9ade10f0de433afd511d708a475ad401
Erik Jansen <erik.gunnar.jansen@nav.no>
2019-11-20 12:07:18 +0100

    Remove ledetekster from Svarside

--- js/components/dialogmoter/dialogmoteplanlegger/Svarside.js
+++ js/components/dialogmoter/dialogmoteplanlegger/Svarside.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import Alertstripe from 'nav-frontend-alertstriper';
-import { getLedetekst } from '@navikt/digisyfo-npm';
 import Svarskjema from './Svarskjema';
 import { motePt } from '../../../propTypes';
 import {
@@ -9,6 +8,16 @@ import {
 } from '../../../utils/moteplanleggerUtils';
 import { visKortDato } from '../../../utils/datoUtils';
 
+const texts = {
+    title: 'Tidspunkt for dialogmøte',
+    info: 'Du har blitt tilsendt nye tidspunkter for dialogmøte',
+};
+
+const getTextConfirmation = (mote) => {
+    const dateBekreftet = visKortDato(finnNyesteAlternativ(mote.alternativer).created);
+    return `Sendt: ${dateBekreftet}`;
+};
+
 const Svarside = (props) => {
     const {
         mote,
@@ -16,19 +25,15 @@ const Svarside = (props) => {
 
     return (<div>
         <header className="sidetopp">
-            <h1 className="sidetopp__tittel">{getLedetekst('mote.svarside.tittel')}</h1>
+            <h1 className="sidetopp__tittel">{texts.title}</h1>
         </header>
         { mote.status === BEKREFTET &&
         <div className="blokk panel">
             <Alertstripe
                 type="info"
                 className="panel">
-                <p className="uthevet">{getLedetekst('mote.svarside.tidligere.bekreftet.info')}</p>
-                <span>
-                    {getLedetekst('mote.svarside.tidligere.bekreftet.sendtdato', {
-                        '%BEKREFTET_DATO%': visKortDato(finnNyesteAlternativ(mote.alternativer).created),
-                    })}
-                </span>
+                <p className="uthevet">{texts.info}</p>
+                <span>{getTextConfirmation(mote)}</span>
             </Alertstripe>
         </div>
         }
