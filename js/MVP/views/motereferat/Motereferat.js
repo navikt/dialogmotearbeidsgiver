ead3ec27870abb1402da3c3219933c10d7480212
Luan Tran <8613937+luantr@users.noreply.github.com>
2021-10-04 00:55:03 +0200

    extract content provider to separat component in motereferat

--- js/MVP/views/motereferat/Motereferat.js
+++ js/MVP/views/motereferat/Motereferat.js
@@ -1,29 +1,17 @@
-import React, { useState } from 'react';
-import styled from 'styled-components';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { Knapp } from 'nav-frontend-knapper';
 import AppSpinner from '../../../components/AppSpinner';
-import Icon from '../../components/Icon';
 import DialogmoteContainer from '../../containers/DialogmoteContainer';
-import DocumentContainer from '../../containers/DocumentContainer';
 import { brevTypes } from '../../globals/constants';
 import { referatBreadcrumb } from '../../globals/paths';
 import { useBrev } from '../../queries/brev';
 import { useSykmeldt } from '../../queries/sykmeldt';
-import { downloadBrevPdf, getProgrammaticDateFormat } from '../../utils';
-import LinkInfoBox from './components/LinkInfoBox';
-import NoReferatAlert from './components/NoReferatAlert';
-import VeilederReferat from './components/VeilederReferat';
+import { getProgrammaticDateFormat } from '../../utils';
 import FeilAlertStripe from '../../components/FeilAlertStripe';
-
-const KnappStyled = styled(Knapp)`
-  margin-top: 32px;
-  width: fit-content;
-`;
+import MotereferatContent from './components/MotereferatContent';
 
 const texts = {
   title: 'Referat fra dialogmøte',
-  button: 'LAST NED PDF',
 };
 
 const getReferat = (brev, date) => {
@@ -46,16 +34,11 @@ const getReferat = (brev, date) => {
   return referat;
 };
 
-const getDocumentKeys = (document) => {
-  return document.filter(({ key }) => key).map(({ key }) => key);
-};
-
 const Motereferat = ({ params }) => {
   const { koblingId, date } = params;
 
   const brev = useBrev(koblingId);
   const sykmeldt = useSykmeldt(koblingId);
-  const [downloadingPDF, setDownloadingPDF] = useState(false);
 
   if (brev.isLoading || sykmeldt.isLoading) {
     return <AppSpinner />;
@@ -69,41 +52,11 @@ const Motereferat = ({ params }) => {
     );
   }
 
-  const handleClick = async (uuid) => {
-    setDownloadingPDF(true);
-    try {
-      await downloadBrevPdf(uuid);
-    } finally {
-      setDownloadingPDF(false);
-    }
-  };
-
   const referat = getReferat(brev.data, date);
-
-  let content;
-  if (!referat) {
-    content = <NoReferatAlert />;
-  } else {
-    const { uuid, document } = referat;
-
-    content = (
-      <React.Fragment>
-        <DocumentContainer document={document} />
-
-        <KnappStyled onClick={() => handleClick(uuid)} autoDisableVedSpinner spinner={downloadingPDF} mini>
-          <Icon icon="download" rightPadding="8px" />
-          {texts.button}
-        </KnappStyled>
-
-        <LinkInfoBox documentKeys={getDocumentKeys(document)} />
-        <VeilederReferat />
-      </React.Fragment>
-    );
-  }
 
   return (
     <DialogmoteContainer title={texts.title} breadcrumb={referatBreadcrumb(sykmeldt)} displayTilbakeknapp>
-      {content}
+      <MotereferatContent referat={referat} />
     </DialogmoteContainer>
   );
 };
---
+++ js/MVP/views/motereferat/components/MotereferatContent.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import { Knapp } from 'nav-frontend-knapper';
+import NoReferatAlert from './NoReferatAlert';
+import { downloadBrevPdf } from '../../../utils';
+import DocumentContainer from '../../../containers/DocumentContainer';
+import Icon from '../../../components/Icon';
+import LinkInfoBox from './LinkInfoBox';
+import VeilederReferat from './VeilederReferat';
+
+const texts = {
+  button: 'LAST NED PDF',
+};
+
+const KnappStyled = styled(Knapp)`
+  margin-top: 32px;
+  width: fit-content;
+`;
+
+const getDocumentKeys = (document) => {
+  return document.filter(({ key }) => key).map(({ key }) => key);
+};
+
+const MotereferatContent = ({ referat }) => {
+  const [downloadingPDF, setDownloadingPDF] = useState(false);
+
+  const handleClick = async (uuid) => {
+    setDownloadingPDF(true);
+    try {
+      await downloadBrevPdf(uuid);
+    } finally {
+      setDownloadingPDF(false);
+    }
+  };
+
+  if (!referat) {
+    return <NoReferatAlert />;
+  }
+  const { uuid, document } = referat;
+
+  return (
+    <React.Fragment>
+      <DocumentContainer document={document} />
+
+      <KnappStyled onClick={() => handleClick(uuid)} autoDisableVedSpinner spinner={downloadingPDF} mini>
+        <Icon icon="download" rightPadding="8px" />
+        {texts.button}
+      </KnappStyled>
+
+      <LinkInfoBox documentKeys={getDocumentKeys(document)} />
+      <VeilederReferat />
+    </React.Fragment>
+  );
+};
+
+MotereferatContent.propTypes = { referat: PropTypes.Object };
+
+export default MotereferatContent;
