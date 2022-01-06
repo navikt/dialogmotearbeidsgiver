fcf8f1409a399f3594ad734b681ffd8738aedada
Kaja Kvinge Akselsen <kaja.kvinge.akselsen@nav.no>
2022-01-06 13:26:05 +0100

    Typing sykmeldte

--- js/MVP/queries/sykmeldte.ts
+++ js/MVP/queries/sykmeldte.ts
@@ -1,8 +1,11 @@
+import { get } from '@/api/axios';
+import { Sykmeldt } from '@/api/types/sykmeldteTypes';
+import { getSykmeldteUrl } from '@/MVP/globals/paths';
 import { useQuery } from 'react-query';
-import { getSykmeldt } from '@/MVP/services/sykmeldte';
 
 const SYKMELDTE = 'sykmeldte';
 
 export const useSykmeldte = (narmestelederId: string) => {
-  return useQuery(SYKMELDTE, () => getSykmeldt(narmestelederId));
+  const fetchSykmeldte = () => get<Sykmeldt>(getSykmeldteUrl(narmestelederId));
+  return useQuery(SYKMELDTE, fetchSykmeldte);
 };
